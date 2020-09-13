8a162f9d968e54f34ab9477394c064da340a4b40
basensio <basensio@basensio-VirtualBox>
2020-09-13 17:52:29 +0200

    Minor changes

--- src/main/java/microservices/registration/main/RegistrationServer.java
+++ src/main/java/microservices/registration/main/RegistrationServer.java
@@ -22,7 +22,6 @@ public class RegistrationServer {
      * Run the application using Spring Boot
      * 
      * @param args Program arguments - ignored.
-     * @throws UnknownHostException 
      */
 	
     public static void main(String[] args) {
